c186136addd14d6e1abe458b1f6cdb058a7f9d3d
songhahaha66 <songhahaha66@qq.com>
2025-08-27 01:24:14 +0800

    实现侧边栏折叠功能，优化布局和样式

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import { ElContainer,ElHeader,ElAside,ElMain, ElButton, ElScrollbar, ElMenu, ElMenuItem, ElPageHeader } from 'element-plus';
 import { useRoute } from 'vue-router';
+import { ref } from 'vue';
 import router from './router';
+
 const route = useRoute();
+const isCollapsed = ref(true); // 默认折叠
+
+const toggleSidebar = () => {
+  isCollapsed.value = !isCollapsed.value;
+};
+
 try{
   const access_token = localStorage.getItem('access_token');
   const refresh_token = localStorage.getItem('refresh_token');
@@ -17,13 +25,32 @@ try{
 <template>
  <div class="common-layout" v-if="route.path !== '/login'">
   <ElContainer>
-   <ElHeader class="main_header">超星学习通作业查询系统</ElHeader>
+   <ElHeader class="main_header">
+     <ElButton 
+       type="text" 
+       @click="toggleSidebar" 
+       class="collapse-btn"
+       size="large"
+     >
+       {{ isCollapsed ? '☰' : '✕' }}
+     </ElButton>
+     <span class="header-title">超星学习通作业查询系统</span>
+   </ElHeader>
     <ElContainer>
-    <ElAside>
-      <ElScrollbar> 
-        <ElMenu :router="true" default-active="/">
-          <ElMenuItem index="/">主页</ElMenuItem>
-          <ElMenuItem index="about">关于</ElMenuItem>
+    <ElAside :width="isCollapsed ? '0px' : '15%'" :style="{ overflow: 'hidden' }">
+      <ElScrollbar v-if="!isCollapsed"> 
+        <ElMenu 
+          :router="true" 
+          default-active="/" 
+          :collapse="false"
+          :collapse-transition="false"
+        >
+          <ElMenuItem index="/">
+            <template #title>主页</template>
+          </ElMenuItem>
+          <ElMenuItem index="about">
+            <template #title>关于</template>
+          </ElMenuItem>
         </ElMenu>
       </ElScrollbar>
     </ElAside>
@@ -41,17 +68,26 @@ try{
 <style scoped>
 .el-header {
   display: flex;
-  align-items: center;   /* 垂直居中 */
-  justify-content: flex-start; /* 内容靠左 */
-  text-align: left;      /* 保持左对齐 */
+  align-items: center;
   font-size: large;
   width: 100%;
   height: 100%;
-  line-height: 100%;
 }
-.el-aside {
-  width: 15%;
+
+.collapse-btn {
+  margin-right: 12px;
+  color: #409eff;
 }
+
+.collapse-btn:hover {
+  background-color: #ecf5ff;
+}
+
+.header-title {
+  font-size: large;
+  font-weight: 600;
+}
+
 .common-layout {
   position: fixed;
   top: 0;
@@ -59,6 +95,7 @@ try{
   height: 100vh;
   width: 100vw;
 }
+
 .login-layout {
   position: fixed;
   top: 0;
@@ -69,9 +106,9 @@ try{
   align-items: center;
   justify-content: center;
 }
+
 .main_header {
   border: 1px solid var(--el-border-color);
   height: 4rem;
 }
-
 </style>
